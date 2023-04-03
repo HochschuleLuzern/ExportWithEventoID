dcf83e3d8ecdd3c4264aad28033f6c07352cf817
msalterhslu <mark.salter@hslu.ch>
2023-04-03 13:18:34 +0200

    Updated version info for Ilias 8

--- plugin.php
+++ plugin.php
@@ -8,14 +8,14 @@
 $id = "xmcexp";
  
 // code version; must be changed for all code changes
-$version = "0.1";
+$version = "0.2";
  
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "7.0";
-$ilias_max_version = "7.999";
+$ilias_max_version = "8.999";
  
 // optional, but useful: Add one or more responsible persons and a contact email
-$responsible = "Stephan Winiker";
-$responsible_mail = "stephan.winiker@hslu.ch";
+$responsible = "Mark Salter";
+$responsible_mail = "mark.salter@hslu.ch";
 ?>
