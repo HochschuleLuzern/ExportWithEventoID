9e972cb76d40fca79e5f88fa377e09ccb4aadb43
msalterhslu <mark.salter@hslu.ch>
2025-04-15 14:58:45 +0200

    Compatibility with ILIAS 9

--- plugin.php
+++ plugin.php
@@ -8,12 +8,12 @@
 $id = "xmcexp";
  
 // code version; must be changed for all code changes
-$version = "0.2";
+$version = "0.3";
  
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = "7.0";
-$ilias_max_version = "8.999";
+$ilias_min_version = "8.0";
+$ilias_max_version = "9.999";
  
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Mark Salter";
