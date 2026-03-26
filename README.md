0e85e1c31cb0acbd83af2fa0e0a97d4c4e075f12
Katalin Lukacsne Toth <katalin.lukacsne@hslu.ch>
2026-03-26 16:25:47 +0100

    Compatibility with ILIAS 10

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ Copyright 2018 Zentrum für Lernen und Lernen, Hochschule Luzern, GPLv3 or later
 
 A very small text export plugin adding our internal ids to the export. Most probably not useful to anyone else, except maybe as a simple implementation for the test export plugin slot.
 
-To be put under Customizing/global/plugins/Modules/Test/Export/
+To be put under public/Customizing/global/plugins/Modules/Test/Export/
--- plugin.php
+++ plugin.php
@@ -8,14 +8,9 @@
 $id = "xmcexp";
  
 // code version; must be changed for all code changes
-$version = "0.3";
+$version = "0.4";
  
-// ilias min and max version; must always reflect the versions that should
+// ILIAS min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = "8.0";
-$ilias_max_version = "9.999";
- 
-// optional, but useful: Add one or more responsible persons and a contact email
-$responsible = "Mark Salter";
-$responsible_mail = "mark.salter@hslu.ch";
-?>
+$ilias_min_version = "10.0";
+$ilias_max_version = "10.999";
